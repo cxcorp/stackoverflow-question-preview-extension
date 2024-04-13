27690b0c5f7c7657342b9c1818e1f2ef6d5dd27f
Joona Heikkilä <21111572+cxcorp@users.noreply.github.com>
2024-04-13 03:18:17 +0300

    Refactor useTooltipHover to use hooks

--- src/scripts/components/useTooltipHover.ts
+++ src/scripts/components/useTooltipHover.ts
@@ -1,48 +1,51 @@
-import { useEffect } from "react";
+import { useCallback, useEffect, useRef } from "react";
 
 export const useTooltipHover = (
   anchors: HTMLAnchorElement[],
   onEnter: (e: HTMLAnchorElement) => void,
   onLeave: () => void
 ) => {
-  useEffect(() => {
-    let enterTimeoutHandle: number | null = null;
+  const enterTimeoutHandle = useRef<number | null>(null);
 
-    const cancelEnterTimer = () => {
-      if (enterTimeoutHandle !== null) {
-        clearTimeout(enterTimeoutHandle);
-        enterTimeoutHandle = null;
-      }
-    };
+  const cancelEnterTimer = useCallback(() => {
+    if (enterTimeoutHandle.current !== null) {
+      clearTimeout(enterTimeoutHandle.current);
+      enterTimeoutHandle.current = null;
+    }
+  }, []);
 
-    const onMouseEnter = (e: MouseEvent) => {
+  const onTargetMouseEnter = useCallback(
+    (e: MouseEvent) => {
       const target = e.currentTarget;
       if (!target || !(target instanceof HTMLAnchorElement)) {
         return;
       }
 
       cancelEnterTimer();
-      enterTimeoutHandle = setTimeout(() => {
+      enterTimeoutHandle.current = setTimeout(() => {
         cancelEnterTimer();
         onEnter(target);
       }, 500);
-    };
+    },
+    [cancelEnterTimer, onEnter]
+  );
 
-    const onMouseLeave = () => {
-      cancelEnterTimer();
-      onLeave();
-    };
+  const onMouseLeave = useCallback(() => {
+    cancelEnterTimer();
+    onLeave();
+  }, [cancelEnterTimer, onLeave]);
 
+  useEffect(() => {
     for (const anchor of anchors) {
-      anchor.addEventListener("mouseenter", onMouseEnter);
+      anchor.addEventListener("mouseenter", onTargetMouseEnter);
       anchor.addEventListener("mouseleave", onMouseLeave);
     }
 
     return () => {
       for (const anchor of anchors) {
-        anchor.removeEventListener("mouseenter", onMouseEnter);
+        anchor.removeEventListener("mouseenter", onTargetMouseEnter);
         anchor.removeEventListener("mouseleave", onMouseLeave);
       }
     };
-  }, [anchors, onEnter, onLeave]);
+  }, [anchors, onTargetMouseEnter, onMouseLeave, cancelEnterTimer, onLeave]);
 };
